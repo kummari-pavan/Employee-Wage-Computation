1b573597360f04037e4979212143ca4142284b3c
kummari-pavan <kpavan892002@gmail.com>
2024-09-12 08:46:35 +0530

    Logic For Attendance

--- emp.py
+++ emp.py
@@ -1,1 +1,8 @@
 print("Hello ! Welcome to Application")
+#uc 1 Logic For attendance
+import random
+attendance = random.randint(0, 1)
+if attendance == 1:
+    print("Emp Present")
+else:
+    print("No")
